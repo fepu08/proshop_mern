f243447b9d3d0ba93cdea4ec127098272c2b33e3
fepu08 <ferencbesenyei@protonmail.com>
2022-10-22 13:04:24 +0200

    implementing react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,26 @@
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/login" element={<h1>Log In</h1>} />
+            <Route path="/cart" element={<h1>Cart</h1>} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="#home">ProShop</Navbar.Brand>
+          <Navbar.Brand as={NavLink} to="/">
+            ProShop
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="justify-content-end w-100">
-              <Nav.Link href="#cart">
+              <Nav.Link as={NavLink} to="/cart">
                 <i className="fas fa-shopping-cart" />
                 &nbsp;Cart
               </Nav.Link>
-              <Nav.Link href="#home">
+              <Nav.Link as={NavLink} to="/login">
                 <i className="fas fa-user" />
                 &nbsp;Sign In
               </Nav.Link>
