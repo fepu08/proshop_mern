711c3e1b0c8783c56f4b7fb1ba4d40cceca621e1
fepu08 <ferencbesenyei@protonmail.com>
2022-11-19 19:21:50 +0100

    fetching products from react

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -2,14 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
+import axios from 'axios';
 
 const ProductScreen = () => {
   const { id } = useParams();
   const [product, setProduct] = useState({});
 
   useEffect(() => {
-    setProduct(products.find((p) => p._id === Number.parseInt(id)));
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`);
+      setProduct(data);
+    };
+    fetchProduct();
   }, [id]);
 
   if (!product) return <h1>404 - Product Not Found</h1>;
