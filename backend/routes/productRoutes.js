f42f118a997687156d4842a840150bba77055e3f
fepu08 <ferencbesenyei@protonmail.com>
2022-11-23 19:19:07 +0100

    custom error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,19 +6,19 @@ const router = express.Router();
 // @desc    Fetch all products
 // @route   GET /api/products
 // @access  Public
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   try {
     const products = await Product.find({});
     return res.status(200).json(products);
   } catch (err) {
-    return res.status(400).json({ message: 'Bad request' });
+    return next(err);
   }
 });
 
 // @desc    Fetch single products
 // @route   GET /api/products/:id
 // @access  Public
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const product = await Product.findById(req.params.id);
 
@@ -28,7 +28,7 @@ router.get('/:id', async (req, res) => {
       res.status(404).json({ message: 'Product not found' });
     }
   } catch (err) {
-    return res.status(400).json({ message: 'Bad request' });
+    return next(err);
   }
 });
 
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import { handleNotFound, handleError } from './middlewares/errorHandlerMiddleware.js';
 
 import productRoutes from './routes/productRoutes.js';
 
@@ -8,14 +9,18 @@ dotenv.config();
 
 connectDB();
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 const ENV = process.env.NODE_ENV || 'development';
+
+const app = express();
 
 app.get('/', (req, res) => {
   res.send('API is running');
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(handleNotFound);
+app.use(handleError);
 
 app.listen(PORT, console.log(`Server is running on port ${PORT} ${ENV}`));
