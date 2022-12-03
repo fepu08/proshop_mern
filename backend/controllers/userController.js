102120d139421dd114d74b64afca59b70db804dc
fepu08 <ferencbesenyei@protonmail.com>
2022-12-03 17:50:40 +0100

    custom authentication middlware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,4 +25,27 @@ export default class UserController {
       return next(err);
     }
   }
+
+  // @desc    Get user profile
+  // @route   GET /api/users/profile
+  // @access  Private
+  static async getUserProfile(req, res, next) {
+    try {
+      const user = await User.findById(req.user._id);
+      if (user) {
+        res.status(200).json({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user._id),
+        });
+      } else {
+        res.status(404);
+        throw new Error('User not found');
+      }
+    } catch (err) {
+      return next(err);
+    }
+  }
 }
