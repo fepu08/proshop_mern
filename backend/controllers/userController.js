b8650060955df583641d427a251083f09e470ee8
fepu08 <ferencbesenyei@protonmail.com>
2022-12-03 18:08:16 +0100

    user registration & password encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,4 +48,40 @@ export default class UserController {
       return next(err);
     }
   }
+
+  // @desc    Register a new user
+  // @route   POST /api/users
+  // @access  Public
+  static async registerUser(req, res, next) {
+    try {
+      const { name, email, password } = req.body;
+      const userExist = await User.findOne({ email });
+
+      if (userExist) {
+        res.status(400);
+        throw new Error('User already exists');
+      }
+
+      const user = await User.create({
+        name,
+        email,
+        password,
+      });
+
+      if (user) {
+        res.status(201).json({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user._id),
+        });
+      } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+      }
+    } catch (err) {
+      return next(err);
+    }
+  }
 }
