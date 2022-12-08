59eed6a661c44f6d13511f1b78d7c510e095a88f
fepu08 <ferencbesenyei@protonmail.com>
2022-12-08 20:36:03 +0100

    update to paid endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.route('/').post(protect, OrderController.addOrderItems);
 router.route('/:id').get(protect, OrderController.getOrderById);
+router.route('/:id/pay').put(protect, OrderController.updateOrderToPaid);
 
 export default router;
